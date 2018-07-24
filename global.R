701a9273939290a5907c2014281b56845ecf04d1
ghettocounselor <markloessi@gmail.com>
2018-07-23 19:35:08 -0600

    original files

---
+++ global.R
@@ -0,0 +1,28 @@
+# data <- readRDS("~/Data/shinny/googlebubble71820/healthexp.Rds")
+# data <- readRDS("/Users/markloessi/Data/shinny/googlebubble71820/healthexp.Rds")
+# data <- readRDS("healthexpcsv.Rds")
+# data <- readRDS("healthexp.Rds")
+convert("healthexpxlsx.xlsx", "healthexpxlsx.Rds")
+
+data <- readRDS("healthexpxlsx.Rds")
+head(data)
+# data$Region <- factor(data$Region) # less specific
+data$Region <- as.factor(data$Region)
+
+# if you edit the XLXS then use this to convert file to RDS
+# convert("healthexpxlsx.xlsx", "healthexpxlsx.Rds")
+
+# pull data in from XLSX and convert to RDS
+# library(rio) # need rio for import of rds and csv
+# convert("healthexp.Rds", "healthexpxlxs.xlsx") # one fell swoop convert to XLXS
+# convert("healthexpxlsx.xlsx", "healthexpxlsx.Rds") # in dir change file type
+
+# other junk
+# y4 <- import("healthexpcsv4.xlsx") # pull in file
+# export(y4, "healthexpxlsx4.Rds") # kick out as RDS
+# or multi step process
+# y <- import("healthexp.Rds")
+# y[,1] = NULL; to remove the 1st column
+# export(y, "healthexpcsv.Rds") # name whatever you want
+
+# data <- read.csv("healthexpCSV.csv") # didn't work ??
